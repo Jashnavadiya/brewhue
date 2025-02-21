666067035e31b4c33338a24c3ab52b4ac819008c
Jashnavadiya <jashnavadiya07@gmail.com>
2025-02-21 15:14:25 +0530

    3

---
+++ frontend/build/static/js/727.4a07c176.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[727],{5727:(e,n,f)=>{f.r(n),f.d(n,{default:()=>r});var r=f(82).l}}]);
+//# sourceMappingURL=727.4a07c176.chunk.js.map
